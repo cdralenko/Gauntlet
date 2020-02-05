0e0defb85f7c900d2fc2eece70ca049c6a2f6943
cdralenko <ainsleeflowers@knights.ucf.edu>
2020-02-05 15:50:09 -0500

    Menu & Levels

---
+++ Gauntlet/Assets/Scripts/Blinker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinker : MonoBehaviour
+{
+    private Text text;
+    private bool isBlinking;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        StartBlinking();
+        isBlinking = true;
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (text.color.a.ToString())
+            {
+                case "0":
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case "1":
+                    text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+                    yield return new WaitForSeconds(0.4f);
+                    break;
+            }
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopCoroutine("Blink");
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopCoroutine("Blink");
+    }
+
+    void Update()
+    {
+        if (!isBlinking)
+        {
+            StartBlinking();
+        }
+    }
+}
